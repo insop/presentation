4aaac59a96a99fdc7b9f74623dabc22ff62bd375
Insop Song <insop.song@gainspeed.com>
2014-04-24 23:10:45 -0700

    Add mkuimage.sh

---
+++ elc_14/scripts/mkuimage.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+#
+# from: http://blog.harrylau.com/2009/08/generate-uboot-uimage.html
+# 'make uImage' can do the same job
+#
+# convert vmlinuz to u-boot readable uImage
+# usage>
+#	./mkuimage.sh note	# note: comment to add to file name
+#
+
+DATE=`date +"%F-%H-%M"`
+BLD_VER=`cat .version`
+if [ "$#" -ne 1 ]; then
+	NOTE=''
+else
+	NOTE="-$1"
+fi
+
+powerpc-linux-objcopy -O binary \
+                                 -R .note -R .comment \
+                                 -S vmlinux linux.bin
+
+gzip -9 linux.bin
+NAME=uImage.$DATE-$USER.u-boot-$BLD_VER$NOTE
+VNAME=vmlinux.$DATE-$USER-$BLD_VER$NOTE
+mkimage -A ppc -O linux -T kernel -C gzip \
+                -a 0 -e 0 -n "Linux Kernel Image" \
+                -d linux.bin.gz $NAME
+
+rm -f linux.bin.gz
+cp $NAME uImage.u-boot
+cp .config config.$NAME
+cp vmlinux $VNAME
+
+echo scp $NAME
